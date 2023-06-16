b73555d3f64f903fa0ec462380beec99968c7f59
Josue013 <josuepinto013@gmail.com>
2023-06-16 14:14:58 -0600

    Primer commit

---
+++ src/Profesores/Alumno.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Profesores;
+
+/**
+ *
+ * @author Josue Hurtarte
+ */
+public class Alumno {
+    
+    public String codigo;
+    public String nombre;
+    public String apellido;
+    public String Acciones;
+
+    public Alumno(String codigo, String nombre, String apellido, String Acciones) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.Acciones = Acciones;
+    }
+    
+    
+    
+}
